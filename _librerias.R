a6db4746e81a88187d596c9ac9b3a0a350ee5d75
Pau Orive Vidal <pauorivevidal@gmail.com>
2026-02-23 12:43:08 +0100

    Actualizo lista de librerías

--- _librerias.R
+++ _librerias.R
@@ -7,6 +7,12 @@ library(data.table)   # para manipulación de datos más eficiente (datasets gra
 
 # importar y exportar datos
 library(arrow)        # para leer y guardar ficheros .parquet 
+library(readxl)       # para leer ficheros de MS Excel
+library(writexl)      # para escribir ficheros de MS Excel .xlsx
+library(RMySQL)          # interfaz para importar datos de MySQL
+library(httr)            # para descargar datos desde APIs
+library(jsonlite)        # para descargar, manejar y escribir JSON
+library(haven)           # para cargar datos de otros softwares estadísticos 
 
 # datos geoespaciales
 library(sf)           
@@ -19,3 +25,19 @@ library(naniar)       # para entender, visualizar e imputar missings
 library(visdat)       # para visualizaciones de la distribución de los missings
 library(rebus)        # para especificar regular expressions de una forma más legible
 
+# estadística y econometría
+library(moments)      # para calcular momentos de la distribución y realizar tests
+library(survivial)    # para análisis de supervivencia
+
+# data science
+
+
+# otras
+library(microbenchmark)  # para comparar rendimiento de diferentes funciones 
+
+
+# Options
+options(
+  pillar.width = 80,    # para que el output 
+  scipen       = 999   
+)
